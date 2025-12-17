8b9cde878e971457ea49ef5981440b41e5ced908
musabbirsammak <sammakhasanmusabbir@gmail.com>
2025-12-17 11:35:44 +0100

    feat: add ChannelInfo for defining channel metadata

--- src/abfscreener/io/abf_loader.py
+++ src/abfscreener/io/abf_loader.py
@@ -9,3 +9,15 @@ import pyabf
 
 class ABFLoadError(RuntimeError):
     """Raised when an ABF file cannot be loaded or validated."""
+
+
+@dataclass(frozen=True)
+class ChannelInfo:
+    """Information about different channels. There might be many different
+    channels used in a Voltage Patch-Clamp experiment such as Current, Voltage, and
+    some other less used channels like Digital/TTL channel, Auxiliary/analog
+    input channels, etc."""
+
+    index: int  # If the file has N channels, they are usually indexed as 0, ... N-1
+    name: str  # A human representable name for the channel
+    units: str

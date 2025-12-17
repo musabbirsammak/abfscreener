d61de6cb360e69eb4d8e9a7cf334274e0d95c22d
musabbirsammak <sammakhasanmusabbir@gmail.com>
2025-12-17 11:45:20 +0100

    feat: add ABFSession wrapper for loaded ABF files.

--- src/abfscreener/io/abf_loader.py
+++ src/abfscreener/io/abf_loader.py
@@ -21,3 +21,24 @@ class ChannelInfo:
     index: int  # If the file has N channels, they are usually indexed as 0, ... N-1
     name: str  # A human representable name for the channel
     units: str
+
+
+@dataclass
+class ABFSession:
+    """
+    Thin wrapper around a loaded and decoded pyabf.ABF with cached metadata and channel mapping.
+    """
+
+    path: Path
+    abf: pyabf.ABF
+    sample_rate_hz: float
+    sweep_count: int
+
+    # The primary measurement channel
+    current_channel: ChannelInfo
+    # Command channel like voltage in Voltage-Clamp protocl.
+    # May not be present in experiments like Current-Clamp.
+    command_channel: Optional[ChannelInfo]
+
+    # any other metadata that user thinks might be useful
+    meta: Dict[str, Any]

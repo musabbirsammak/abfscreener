55aadcb1e6a3f92b880493bc1d98448863c53b9b
musabbirsammak <sammakhasanmusabbir@gmail.com>
2025-12-18 08:25:07 +0100

    feat: adds SweepIndexError, a generic error that can be raised whenever there is a problem reading sweep data.

---
+++ src/abfscreener/io/sweep_indexer.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Iterator, List, Optional, Tuple
+
+import numpy as np
+
+from .abf_loader import ABFSession
+
+
+class SweepIndexError(RuntimeError):
+    """Raised when sweeps or epochs cannot be indexed reliably."""
